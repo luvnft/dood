df83130075497784c344c45cad1340e853fb3874
The Wizard of Hahz <info@luvnft.com>
2024-08-19 13:26:30 -0400

    updated

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -3,7 +3,7 @@ import { SITENAME } from "@/lib/constants";
 const navigation = [
     {
         name: "Instagram",
-        href: "https://instagram.com/atl5d",
+        href: "https://instagram.com/blvetv",
         icon: (props: any) => (
             <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                 <path
@@ -16,7 +16,7 @@ const navigation = [
     },
     {
         name: "Twitter",
-        href: "https://x.com/atl5d",
+        href: "https://x.com/lovecash",
         icon: (props: any) => (
             <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -24,18 +24,19 @@ const navigation = [
         ),
     },
     {
-        name: "YouTube",
-        href: "https://youtube.com/atl5d",
+        name: "HAHZ",
+        href: "https://hahz.arvrtise.com, // Replace with your Telegram link
         icon: (props: any) => (
-            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-                <path
-                    fillRule="evenodd"
-                    d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-                    clipRule="evenodd"
-                />
-            </svg>
+          <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+            <path
+              fillRule="evenodd"
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.29 8.29L9.77 16.11c-.3.29-.68.44-1.09.44-.11 0-.22-.01-.34-.04-.57-.15-1.06-.52-1.34-1.02l-1.63-2.88 8.59-3.53c.26-.11.54-.17.83-.17.58 0 1.07.3 1.33.79.28.52.15 1.12-.33 1.53zM9.99 14.2l.56.99c.12.21.33.32.56.32.06 0 .11-.01.17-.03l4.64-2.85c.18-.11.23-.36.1-.54-.13-.18-.38-.23-.56-.1l-4.24 2.6-.68-.77z"
+              clipRule="evenodd"
+            />
+          </svg>
         ),
-    },
+      },
+      
 ];
 
 export default function Footer() {
